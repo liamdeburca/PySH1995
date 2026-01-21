a1947dd4f3fb38f33d7dec1495c0d4f00d7486ef
Liam M.E. de Burca <liam_deburca@me.com>
2026-01-21 13:56:50 +0100

    Minor fix.

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@
 Module for creating, manipulating, and retrieving data from Storey & Hummer 
 (1995) in an elegant, user-friendly, pythonic way.
 """
-from src.utils.reading import Query
+from .src.utils.reading import Query
